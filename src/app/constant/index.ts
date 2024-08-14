9771a70706d492166bd534b2e36d5c6e881501e2
HikenNoAce001 <zobairf03@gmail.com>
2024-08-14 23:29:32 +0600

    feat:background & socials link

---
+++ src/app/constant/index.ts
@@ -0,0 +1,14 @@
+import { link } from 'fs';
+
+export const Socials = [
+  {
+    name: 'Github',
+    src: '/github.svg',
+    link: 'https://github.com/HikenNoAce001',
+  },
+  {
+    name: 'Linkedin',
+    src: '/linkedin.svg',
+    link: 'www.linkedin.com/in/fahim77108',
+  },
+];
