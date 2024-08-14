60abcc5770599efc809a39a911c6759b13016f9c
HikenNoAce001 <zobairf03@gmail.com>
2024-08-14 16:23:35 +0600

    feat:tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,32 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      fontSize: {
+        xs: ['12px', { lineHeight: '15.6px' }],
+        sm: ['14px', { lineHeight: '18.2px' }],
+        base: ['16px', { lineHeight: '24px' }],
+        lg: ['18px', { lineHeight: '27px' }],
+        h8: ['14px', { lineHeight: '21px' }],
+        h7: ['16px', { lineHeight: '24px' }],
+        h6: ['18px', { lineHeight: '27px' }],
+        h5: ['20px', { lineHeight: '30px' }],
+        h4: ['24px', { lineHeight: '36px' }],
+        h3: ['30px', { lineHeight: '42px' }],
+        h2: ['36px', { lineHeight: '54px' }],
+        h1: ['60px', { lineHeight: '66px' }],
+        header: ['52px', { lineHeight: '66px' }],
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
