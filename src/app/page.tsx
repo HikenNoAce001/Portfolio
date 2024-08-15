abf383391686360ffb62e65870cc4d6a651bd269
Beasty356 <zobairf03@gmail.com>
2024-08-15 22:20:36 +0600

    fix:responsive design issue for social

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@ import Social from './Components/socials';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen space-y-5 bg-gradient-to-tl from-slate-900 to-gray-500">
-      <Social />
-      <div className=" flex-col items-center justify-between p-24">
+    <main className="flex flex-auto w-screen h-screen space-y-5 bg-gradient-to-tl from-slate-900 to-gray-500">
+      <div className="absolute top-0 right-0 p-8">
+        <Social />
+      </div>
+      <div className="flex flex-col items-center justify-between p-24">
         <div className="border-2 border-x-4 border-y-4 shadow-2xl rounded-3xl p-7 bg-black">
           {' '}
           <div className="text-h1">
