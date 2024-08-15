f9bf1520e617ad44b233ffcf458675e992240e5f
Fahim <49021952+HikenNoAce001@users.noreply.github.com>
2024-08-16 00:27:30 +0600

    feat:tailwind configuration (#1)
    * feat:tailwind configuration
    
    * feat:background & socials link
    
    * feat:background color
    
    * fix:responsive design issue for social

---
+++ src/app/Components/socials.tsx
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
+export const Socials = [
+  {
+    name: 'Github',
+    src: '/github.svg',
+    link: 'https://github.com/HikenNoAce001',
+  },
+  {
+    name: 'Linkedin',
+    src: '/linkedin.svg',
+    link: 'https://www.linkedin.com/in/fahim77108/',
+  },
+];
+
+export default function Social() {
+  return (
+    //bg-gradient-to-r from-[#7058b9] to-[#04013d]
+    <div className="flex flex-row gap-5 p-5">
+      {Socials.map((social) => (
+        <Link
+          key={social.name}
+          href={social.link}
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          <Image
+            src={social.src}
+            alt={social.name}
+            width={40}
+            height={40}
+            className="transition-transform transform hover:scale-110"
+          />
+        </Link>
+      ))}
+    </div>
+  );
+}
---
+++ src/app/constant/index.ts
@@ -0,0 +1,12 @@
+export const Socials = [
+  {
+    name: 'Github',
+    src: '/github.svg',
+    link: 'https://github.com/HikenNoAce001',
+  },
+  {
+    name: 'Linkedin',
+    src: '/linkedin.svg',
+    link: 'https://www.linkedin.com/in/fahim77108/',
+  },
+];
