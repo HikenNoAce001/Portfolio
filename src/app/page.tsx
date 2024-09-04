f92424676e7063631439c3ac6f35f74ab8c4bcda
Fahim <49021952+HikenNoAce001@users.noreply.github.com>
2024-09-04 20:04:47 +0600

    Refactor Code of homepage (#3)
    * feat:tailwind configuration
    
    * feat:background & socials link
    
    * feat:background color
    
    * fix:responsive design issue for social
    
    * feat:text position styling
    
    * refactor:minor changes in page(colors and stuff)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,15 @@ import Social from './Components/socials';
 
 export default function Home() {
   return (
-    <main className="flex flex-auto w-screen h-screen space-y-5 bg-gradient-to-tl from-slate-900 to-gray-500">
-      <div className="absolute top-0 right-0 p-8">
+    <main className="relative flex flex-col w-screen h-screen justify-center bg-[#0f172a]">
+      <div className="absolute top-0 right-0 p-7">
         <Social />
       </div>
-      <div className="flex flex-col items-center justify-between p-24">
-        <div className="border-2 border-x-4 border-y-4 shadow-2xl rounded-3xl p-7 bg-black">
-          {' '}
-          <div className="text-h1">
-            Hello
-            <p className="text-h3">Portfolio skeleton on build</p>
-          </div>
+      <div className="flex flex-col items-center p-24">
+        {' '}
+        <div className="text-h1 items-center">
+          Mohammad Zobair Hossain Fahim
+          <p className="text-h3 items-center">Portfolio skeleton on build</p>
         </div>
       </div>
     </main>
