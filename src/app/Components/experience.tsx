5224e77db8704c1b8aca9de98b67e8c483e590f0
HikenNoAce001 <zobairf03@gmail.com>
2024-09-13 09:54:15 +0600

    fix:testing deployment

--- src/app/Components/experience.tsx
+++ src/app/Components/experience.tsx
@@ -15,17 +15,7 @@ export default function Experience() {
               understanding user requirements, and ensuring the delivery of
               high-quality software products. This experience gave me a strong
               foundation in testing and a keen eye for detail, which I now bring
-              into my development work. For the past 6 months, I’ve transitioned
-              into a developer role, where I’ve been actively contributing to
-              building and maintaining applications. My time in QA has given me
-              a unique perspective as a developer, allowing me to write cleaner,
-              more efficient code with the end-user in mind. I&lsquo;m
-              passionate about coding and am always eager to learn new
-              technologies and best practices to enhance my skills further. I
-              thrive in environments where learning is encouraged, and I’m
-              always open to feedback that helps me grow as a developer. My goal
-              is to continue developing my technical expertise while
-              contributing positively to my team and the projects I work on.
+              into my development work.
             </p>
           </div>
         </div>
